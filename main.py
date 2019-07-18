b3a02f6f59fd3ca3687f14ba64d0910eea4ddd24
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-18 10:36:52 -0400

    TRIED A POST METHOD

--- main.py
+++ main.py
@@ -12,6 +12,24 @@ class Home(webapp2.RequestHandler):
         template = the_jinja_enviorment.get_template('/templates/index.html')
         self.response.headers['Content-Type'] = 'text/html'
         self.response.write(template.render())
+class BlogHandler(webapp2.RequestHandler):
+    def get(self):
+        template = the_jinja_enviorment.get_template('/templates/new_post.html')
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(template.render())
+    def post(self):
+        post_title = self.request.get('postTitle')
+        post_content = self.request.get('postContent')
+        Name = self.request.get('aurthorName')
+
+        template_vars = {
+        'post title':
+        'post content':
+        'Name': 
+        }
+
+
 app = webapp2.WSGIApplication([
     ('/', Home),
+    ('/blogmake', BlogHandler),
 ], debug=True)
